1002b83f64979e70775b7a506f089002d022e441
Evgeny39rus <denver417@gmail.com>
2024-01-18 20:41:58 +0200

    edit_any_func
    удалили несколько функций

--- Homework10v2/ViewController.swift
+++ Homework10v2/ViewController.swift
@@ -1,11 +1,9 @@
 import UIKit
-
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
     @IBAction  func changeColor(_ sender: Any) {
             let navBar = self.navigationController?.navigationBar
             navBar?.barTintColor = .red
