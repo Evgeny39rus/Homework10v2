ec55b391cc3ac2e0e00e133d477a0a17890aac9b
Evgeny39rus <denver417@gmail.com>
2024-01-18 20:22:54 +0200

    delited_comments
    удалены все лишние строки и комментарии

--- Homework10v2/ViewController.swift
+++ Homework10v2/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  Homework10v2
-//
-//  Created by Евгений Сушков on 18.01.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
